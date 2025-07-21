cba9ed06e2fa8d6d032621b739a85e20a5fa5482
Abdalrahman <abdalrahman.alaa.eldin@gmail.com>
2025-07-22 01:00:11 +0300

    add gradient text app colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color hoverColor = Colors.blueAccent.withValues(alpha: 0.6);
+  static List<Color> gradientTextColors = [
+    Colors.deepPurpleAccent,
+    Colors.blueAccent,
+  ];
 }
