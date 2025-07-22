ff979b2db7b0d246a9102f6c23fddb462124e824
Abdalrahman1723 <Abdalrahman1723@users.noreply.github.com>
2025-07-22 11:05:01 +0000

    deploy: db6fd25c4a00340d7ab6a745fc6f374967700c7f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/lib/core/assets/chatting.jpg": "5dc5be45ea680b895997c
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "4f350faf130ce9c4492a7d4b4b35a861",
 "version.json": "9b818ca9511483c901bed1545384376c",
 "manifest.json": "e2b9103aaafaf8ed9f90576c9d9a1341",
-"flutter_bootstrap.js": "f3b43816439c72f3140a5f0e64f96a3e",
+"flutter_bootstrap.js": "5ade5602c0b99b6884b2002e1fd7aa64",
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "canvaskit/canvaskit.wasm": "7a3f4ae7d65fc1de6a6e7ddd3224bc93",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/lib/core/assets/chatting.jpg": "5dc5be45ea680b895997c
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"main.dart.js": "b66d85290cd4bfc873a459bd1a137a51"};
+"main.dart.js": "e8699cac68439900a2e696a862c16845"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
