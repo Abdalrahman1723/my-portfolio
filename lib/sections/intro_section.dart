75e9e7e0202fda7cb191cd1244b750c1bfd5f99f
Abdalrahman <abdalrahman.alaa.eldin@gmail.com>
2025-07-23 15:24:07 +0300

    fix extension

--- lib/sections/intro_section.dart
+++ lib/sections/intro_section.dart
@@ -52,7 +52,7 @@ class IntroSection extends StatelessWidget {
     double sectionSpacing = isSmall ? 8 : 12;
 
     Widget avatar = CustomCircularAvatar(
-      imageUrl: "lib/core/assets/intro.png",
+      imageUrl: "lib/core/assets/intro.jpg",
       radius: avatarRadius,
     );
 
