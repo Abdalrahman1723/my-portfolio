72329709c4e0d0486f5dd8b0e8f7edf781f80573
Abdalrahman <abdalrahman.alaa.eldin@gmail.com>
2025-07-23 15:13:55 +0300

    add BMI and meals app projects

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -8,7 +8,7 @@ final List<Project> projects = [
     description:
         'A modular Flutter mobile application for a healthcare system, built with Clean Architecture. It supports multiple user roles (Patient, Doctor, Admin) and communicates with a .NET backend API for business logic and data operations.',
     slogan: 'Fiend the suitable doctor for you.',
-    technologies: ['Flutter', 'Dart', '.Net', 'RESTFUL API'],
+    technologies: ['Flutter', 'Dart', '.Net', 'RESTFUL API', "bloc"],
     githubUrl: 'https://github.com/Abdalrahman1723/Health-care-management-app',
     imageURL: 'lib/core/assets/hcms.png',
   ),
@@ -29,7 +29,12 @@ final List<Project> projects = [
     title: 'Personal portfolio',
     slogan: "Nice to meet you",
     description: 'A personal portfolio using flutter web',
-    technologies: ['Flutter', 'Dart', 'web'],
+    technologies: [
+      'Flutter',
+      'Dart',
+      'web'
+          "UI design",
+    ],
     githubUrl: 'https://github.com/Abdalrahman1723/my-portfolio',
     imageURL: 'lib/core/assets/portfolio.png',
   ),
@@ -53,6 +58,26 @@ final List<Project> projects = [
     technologies: ['Flutter', 'Dart'],
     githubUrl: 'https://github.com/Abdalrahman1723/Expenses-app',
     imageURL: 'lib/core/assets/expenses.jpg',
+  ),
+  //--------------------meals app
+  Project(
+    title: 'Meals app',
+    slogan: "What are you cooking today",
+    description:
+        "A simple Flutter app that showcases a variety of recipes across multiple categories. Users can browse, view details, and apply dietary filters such as gluten-free, lactose-free, vegetarian, and vegan meals.",
+    technologies: ['Flutter', 'Dart', "Riverpod"],
+    githubUrl: 'https://github.com/Abdalrahman1723/meals-app',
+    imageURL: 'lib/core/assets/meals.jpg',
+  ),
+  //--------------------BMI app
+  Project(
+    title: 'BMI app',
+    slogan: "Know your Body Mass Index",
+    description:
+        "A simple and modern BMI (Body Mass Index) calculator built using Flutter. The app allows users to input their height and weight to quickly calculate their BMI and get health-related feedback.",
+    technologies: ['Flutter', 'Dart'],
+    githubUrl: 'https://github.com/Abdalrahman1723/BMI-app',
+    imageURL: 'lib/core/assets/bmi.jpg',
   ),
   // Add more projects here
 ];
