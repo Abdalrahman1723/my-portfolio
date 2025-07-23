03d188f65755781814da765db0d12046820f481c
Abdalrahman <abdalrahman.alaa.eldin@gmail.com>
2025-07-23 07:42:30 +0300

    fix avatar issue

--- lib/core/utils/avatar.dart
+++ lib/core/utils/avatar.dart
@@ -2,63 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomCircularAvatar extends StatelessWidget {
   final double radius;
-  final String? imageUrl;
-  final IconData? icon;
-  final Color? backgroundColor;
-  final Color? iconColor;
-  final String? initials;
-  final TextStyle? initialsStyle;
+  final String imageUrl;
 
   const CustomCircularAvatar({
     super.key,
     this.radius = 40,
-    this.imageUrl,
-    this.icon,
-    this.backgroundColor,
-    this.iconColor,
-    this.initials,
-    this.initialsStyle,
+    required this.imageUrl,
   });
 
   @override
   Widget build(BuildContext context) {
-    if (imageUrl != null && imageUrl!.isNotEmpty) {
-      return CircleAvatar(
-        radius: radius,
-        backgroundImage: NetworkImage(imageUrl!),
-        backgroundColor: backgroundColor ?? Colors.grey[300],
-      );
-    } else if (icon != null) {
-      return CircleAvatar(
-        radius: radius,
-        backgroundColor: backgroundColor ?? Colors.grey[300],
-        child: Icon(icon, color: iconColor ?? Colors.white, size: radius),
-      );
-    } else if (initials != null && initials!.isNotEmpty) {
-      return CircleAvatar(
-        radius: radius,
-        backgroundColor: backgroundColor ?? Colors.grey[300],
-        child: Text(
-          initials!,
-          style:
-              initialsStyle ??
-              TextStyle(
-                color: Colors.white,
-                fontSize: radius * 0.6,
-                fontWeight: FontWeight.bold,
-              ),
-        ),
-      );
-    } else {
-      return CircleAvatar(
-        radius: radius,
-        backgroundColor: backgroundColor ?? Colors.grey[300],
-        child: Icon(
-          Icons.person,
-          color: iconColor ?? Colors.white,
-          size: radius,
-        ),
-      );
-    }
+    return CircleAvatar(
+      radius: radius,
+      backgroundImage: AssetImage(imageUrl),
+      backgroundColor: Colors.grey[300],
+    );
   }
 }
